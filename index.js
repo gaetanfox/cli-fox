a85aa4db1a4789bb4880f28e9e7f9155130787d5
Cody McCodePants <cody@mccodepants.com>
2022-01-10 22:45:54 +0100

    Added cli welcome pkg

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
-clearConsole();
+const welcome = require('cli-welcome');
+const pkgJson = require('./package.json');
+
+welcome({
+    title: pkgJson.name,
+    tagLine: 'Get to know Fox',
+    version: pkgJson.version,
+    description: pkgJson.description,
+    bgColor: `#FADC00`,
+    color: '#000000',
+    bold: true,
+    clear: true,
+});
+
 console.log(`Hello I am fox`);
